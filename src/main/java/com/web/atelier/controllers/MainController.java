64a0fcc0b3358d7cd7fb3137dec43d3fe74a3be1
Nelson <61602818+KapNelson@users.noreply.github.com>
2021-05-08 22:35:51 +0300

    Atelier (beta-v.5.1)
    Added processing of requests for registration and authorization of users.

--- src/main/java/com/web/atelier/controllers/MainController.java
+++ src/main/java/com/web/atelier/controllers/MainController.java
@@ -3,11 +3,15 @@ package com.web.atelier.controllers;
 import com.web.atelier.models.User;
 import com.web.atelier.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -21,19 +25,35 @@ public class MainController {
         return "index";
     }
 
-    @PostMapping("/")
-    public String addUser(@RequestParam String login, @RequestParam String password, Model model) {
-        User user = new User(login, password);
-        userRepository.save(user);
-        return "index";
+    @PostMapping("/reg")
+    public ResponseEntity<HttpStatus> regNewUser(@RequestParam String login_new, @RequestParam String password_new, @RequestParam String repassword_new, Model model) {
+        Optional<User> userOpt = userRepository.findById(login_new);
+
+        if(!userOpt.isPresent()) {
+            if (password_new.equals(repassword_new)) {
+                User user = new User(login_new, password_new);
+                userRepository.save(user);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 
-    /*@GetMapping("/")
-    public String test(Model model) {
-        Iterable<User> users = userRepository.findAll();
-        model.addAttribute("users", users);
-        return "index";
-    }*/
-
-
+    @PostMapping("/login")
+    public ResponseEntity<HttpStatus> authorizationUser(@RequestParam String login, @RequestParam String password, Model model) {
+        Optional<User> userOpt = userRepository.findById(login);
+        if(userOpt.isPresent()){
+            User user = userOpt.get();
+            if(user.getPasword().equals(password)) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -124,3 +124,40 @@ $(function () {
         $('.menu a').mPageScroll2id();
     });
 })(jQuery);*/
+
+$('#registration').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+        url: '/reg',
+        type: 'post',
+        data: $('#registration').serialize(),
+        success: function () {
+            document.getElementById("message_reg").innerHTML = "Реєстрація успішна!";
+        },
+        statusCode: {
+            401: function () {
+                document.getElementById("message_reg").innerHTML = "Паролі не співпадають!";
+            },
+            409: function () {
+                document.getElementById("message_reg").innerHTML = "Користувач з таким логіном вже існує!";
+            }
+        }
+    });
+});
+
+$('#login').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+        url: '/login',
+        type: 'post',
+        data: $('#login').serialize(),
+        success: function () {
+            document.getElementById("message").innerHTML = "Вхід успішний!";
+        },
+        statusCode: {
+            401: function () {
+                document.getElementById("message").innerHTML = "Логін або пароль - невірні!";
+            }
+        }
+    });
+});
